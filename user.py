f802de361b441f3f658c76e8d78a8f008d151495
coder_uz <husanjalilov00777@gmil.com>
2022-11-23 19:07:20 +0500

    user all task

--- user.py
+++ user.py
@@ -1,3 +1,4 @@
+import json
 def get_users_by_country(data: dict, country: str) -> list[dict]:
     '''get users by country
     
@@ -8,6 +9,17 @@ def get_users_by_country(data: dict, country: str) -> list[dict]:
     Returns: 
         list: list of users. item ex: {'full_name': f'{first} {last} {title}', 'age': int}
     '''
+   
+    user=[]
+    for i in data['results']:
+
+        if i['location']['country']==country:
+            user.append({
+                'full_name':f"{i['name']['title']} {i['name']['first']} {i['name']['last']}",
+                'age':f"{i['dob']['age']}"
+                })
+    return user
+    
 
 
 def get_users_by_age(data: dict, age: int) -> list[dict]:
@@ -20,6 +32,16 @@ def get_users_by_age(data: dict, age: int) -> list[dict]:
     Returns: 
         list: list of users. item ex: {'full_name': f'{first} {last} {title}', 'age': int}
     '''
+    age1=[]
+    for i in data['results']:
+       
+        if i['dob']['age']==age:
+            
+            age1.append({
+                'full_name':f"{i['name']['title']} {i['name']['first']} {i['name']['last']}",
+                'age':f"{i['dob']['age']}"
+                })
+    return age1
 
 
 def get_users_by_city(data: dict, city: str) -> list[dict]:
@@ -32,6 +54,16 @@ def get_users_by_city(data: dict, city: str) -> list[dict]:
     Returns: 
         list: list of users. item ex: {'full_name': f'{first} {last} {title}', 'age': int}
     '''
+    ct=[]
+    for i in data['results']:
+       
+        if i['location']['city']==city:
+            
+            ct.append({
+                'full_name':f"{i['name']['title']} {i['name']['first']} {i['name']['last']}",
+                'age':f"{i['dob']['age']}"
+                })
+    return ct
 
 
 def get_users_by_nat(data: dict, nat: str) -> list[dict]:
@@ -44,3 +76,14 @@ def get_users_by_nat(data: dict, nat: str) -> list[dict]:
     Returns: 
         list: list of users. item ex: {'full_name': f'{first} {last} {title}', 'age': int}
     '''
+    nt=[]
+    for i in data['results']:
+        
+       
+        if i['nat']==nat:
+            
+            nt.append({
+                'full_name':f"{i['name']['title']} {i['name']['first']} {i['name']['last']}",
+                'age':f"{i['dob']['age']}"
+                })
+    return nt
